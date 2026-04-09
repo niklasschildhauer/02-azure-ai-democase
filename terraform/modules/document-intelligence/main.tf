26a0e7e5ab805b366f35f0015442efa0250f56e1
filiptolovski <filip.tolovski@codecentric.de>
2026-04-09 13:55:23 +0200

    added subdomain
    added subdomain for document intelligence, which is a condition for managed identity identification

--- terraform/modules/document-intelligence/main.tf
+++ terraform/modules/document-intelligence/main.tf
@@ -2,11 +2,12 @@
 # Provides OCR and form recognition capabilities
 
 resource "azurerm_cognitive_account" "doc_intelligence" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  kind                = "FormRecognizer"
-  sku_name            = var.sku_name
+  name                  = var.name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  kind                  = "FormRecognizer"
+  sku_name              = var.sku_name
+  custom_subdomain_name = var.name
 
   tags = var.tags
 }
